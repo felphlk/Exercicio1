66863f6139e28326f69af4f4509f56166b7b3671
felphlk <57629494+felphlk@users.noreply.github.com>
2021-02-19 15:23:38 -0300

    Update Program.java

--- src/Program.java
+++ src/Program.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class a {
+public class Program {
     public static void main(String[] args) {
         System.out.print("Multiplicando: ");
         Scanner a= new Scanner(System.in);
